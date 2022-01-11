4d2e4341051feabeedd33d4af21715553948c8be
ybrooks08 <yosbel88@gmail.com>
2022-01-12 00:37:15 +0100

    Added Slack and README.md adjustments. #5

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/bug-iwvg-devops-brooks-yosbel-2.0.0-RELEASE.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/iwvg-devops-brooks-yosbel-2.0.0-RELEASE.jar
 
--- src/main/java/es/upm/miw/iwvg_devops/plan/SlackWebHook.java
+++ src/main/java/es/upm/miw/iwvg_devops/plan/SlackWebHook.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
+//SlackWebHook class
 @Service
 public class SlackWebHook {
     private static final String
