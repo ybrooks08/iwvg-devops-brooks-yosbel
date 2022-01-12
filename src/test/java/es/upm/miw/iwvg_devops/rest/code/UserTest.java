c41dff29cf5cd34476160da44f9056ff4e01c21f
ybrooks08 <yosbel88@gmail.com>
2022-01-12 22:24:53 +0100

    Added UserDatabase. #14

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
@@ -1,16 +1,8 @@
 package es.upm.miw.iwvg_devops.rest.code;
 
-import es.upm.miw.iwvg_devops.code.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
-    private User user;
-
-    @BeforeEach
-    void before() {
-        user = new User();
-    }
 
     @Test
     void testUserGetId() {
